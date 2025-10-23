06ed42d2087c3254548c117ba9cfd7f25616f3e8
petukhov.vladislav <robertlokam@yandex.ru>
2025-10-23 22:24:58 +0300

    lesson14 HW  Shelf

--- src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson14/homeworks/Shelf.kt
+++ src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson14/homeworks/Shelf.kt
@@ -1,4 +1,67 @@
 package ru.stimmax.ru.stimmax.kotlin_course.lesson.lesson14.homeworks
 
-class Shelf(val capacity: Int,) {
+class Shelf(val capacity: Int, val items: MutableList<String>) {
+
+   // Добавление Предмета (addItem):
+   // Принимает название предмета.
+   // Проверяет, поместится ли предмет на полку, учитывая длину его названия и текущее заполнение полки.
+   // Возвращает true и добавляет предмет, если есть место. В противном случае возвращает false.
+    fun addItems(thing: String): Boolean{
+        if ((capacity - items.sumOf { it.length }) >= thing.length) {
+            items.add(thing)
+            return true
+        } else {
+            return false
+        }
+    }
+    // Удаление Предмета (removeItem):
+    // Принимает название предмета.
+    // Удаляет предмет, если он найден на полке.
+    // Возвращает true, если предмет был удален, и false, если такой предмет не найден.
+    fun removeItem(thing: String): Boolean {
+        if (items.contains(thing)){
+            items.remove(thing)
+            return  true
+        } else {
+            println("$thing не найден")
+            return false
+        }
+    }
+    // Проверка Вместимости (canAccommodate):
+    // Принимает название предмета.
+    // Определяет, вместится ли предмет на полку.
+    // Возвращает true, если предмет вместится, и false, если места недостаточно.
+    fun canAccommodate(thing: String): Boolean {
+        if ((capacity - items.sumOf { it.length }) >= thing.length) {
+            println("$thing вместится на полку")
+            return true
+        } else {
+            println("$thing не вместится на полку")
+            return false
+        }
+    }
+    // Проверка наличия предмета (containsItem):
+    // Принимает название предмета
+    // Возвращает true если такой предмет есть
+    fun containsItem(thing: String): Boolean = items.contains(thing)
+
+    // Получение списка предметов (getItems):
+    // Возвращает неизменяемый список предметов
+    fun getItems(): List<String> = items.toList()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
