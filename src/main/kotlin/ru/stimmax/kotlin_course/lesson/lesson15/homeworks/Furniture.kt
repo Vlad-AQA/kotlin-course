00040bf40ce75ada08259273fde0dbe475bf9404
petukhov.vladislav <robertlokam@yandex.ru>
2025-10-27 18:38:11 +0300

    lesson15 HW part2 fix

--- src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson15/homeworks/Furniture.kt
+++ src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson15/homeworks/Furniture.kt
@@ -1,10 +1,11 @@
 package ru.stimmax.ru.stimmax.kotlin_course.lesson.lesson15.homeworks
 
-
 abstract class Furniture {
-    protected val materials = mutableListOf<String>()
+    val materials = mutableListOf<String>()
 
-    abstract fun addMaterial(material: String)
+    fun addMaterial(material: String) {
+        materials.add(material)
+    }
 
     fun getMaterials(): List<String> {
         return materials
@@ -12,14 +13,14 @@ abstract class Furniture {
 }
 
 class BottomInsertFurniture : Furniture() {
-    override fun addMaterial(material: String) {
+    fun addMaterialToBottom(material: String) {
         materials.add(0, material)
     }
 }
 
 class AlternatingInsertFurniture : Furniture() {
-    override fun addMaterial(material: String) {
-        val newList = mutableListOf<String>()
+
+    fun addMaterialAlternating(material: String) {
         val oldList = materials.toList()
         materials.clear()
 
@@ -41,26 +42,22 @@ class AlternatingInsertFurniture : Furniture() {
 }
 
 class SortedInsertFurniture : Furniture() {
-    override fun addMaterial(material: String) {
+
+    fun addMaterialSorted(material: String) {
         materials.add(material)
         materials.sort()
     }
 }
 
 class KeyValueInsertFurniture : Furniture() {
-    fun addMaterials(map: Map<String, String>) {
-        // Добавляем ключи в начало в обратном порядке
+
+    fun addMaterialsFromMap(map: Map<String, String>) {
         for (key in map.keys.toList().reversed()) {
             materials.add(0, key)
         }
-        // Добавляем значения в конец
         for (value in map.values) {
             materials.add(value)
         }
     }
-
-    override fun addMaterial(material: String) {
-        // Просто добавляем в конец
-        materials.add(material)
-    }
 }
+
