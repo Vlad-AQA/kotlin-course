92363a4f0856e1fa1585a37d1075e1496b62f6a8
petukhov.vladislav <robertlokam@yandex.ru>
2025-11-10 16:23:30 +0300

    lesson18 HW addition

--- src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson18/homeworks/SwitchingEquipment.kt
+++ src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson18/homeworks/SwitchingEquipment.kt
@@ -1,5 +1,6 @@
 package ru.stimmax.ru.stimmax.kotlin_course.lesson.lesson18.homeworks
 
+
 abstract class SwitchingEquipment() : Powerable {
     override fun powerOn() {
         println("Включили")
@@ -29,24 +30,52 @@ abstract class ProgrammableEquipment(
 
 abstract class Equipment(
     action: String,
-    val name: String,
     val maxT: Int
 ) : ProgrammableEquipment(action), TemperatureRegulatable, Openable {
     override fun open() {
-        println("$name открыто")
+        println("Устройство открыто")
     }
 
     override fun close() {
-        println("$name закрыто")
+        println("Устройство закрыто")
     }
 
     override val maxTemperature: Int
         get() = maxT
 
     override fun setTemperature(temp: Int) {
-        powerOn()
         println("Установлена температура $temp")
     }
+}
 
+class Refrigerator(
+    action: String,
+    val serialNumber: String,
+    var inclusion: Boolean = false,
+    var openDoor: Boolean = false
 
+) : Equipment(action, maxT = 10) {
+
+    override fun powerOn() {
+        inclusion = true
+        super.powerOn()
+    }
+
+    override fun setTemperature(temp: Int) {
+        if (inclusion) {
+            super.setTemperature(temp)
+        } else {
+            println("Холодильник не включен")
+        }
+    }
+
+    override fun open() {
+        openDoor = true
+        super.open()
+    }
+
+    override fun close() {
+        openDoor = false
+        super.close()
+    }
 }
