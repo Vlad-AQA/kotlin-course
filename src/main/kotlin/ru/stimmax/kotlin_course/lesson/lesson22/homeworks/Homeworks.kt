efd41f52100c104f5ab030453563a6ac88a0c67e
petukhov.vladislav <robertlokam@yandex.ru>
2025-11-23 23:10:46 +0300

    lesson22 HW 1

---
+++ src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson22/homeworks/Homeworks.kt
@@ -0,0 +1,43 @@
+package ru.stimmax.ru.stimmax.kotlin_course.lesson.lesson22.homeworks
+
+fun main() {
+
+
+// NullPointerException
+//val a = null
+//a!!
+//
+//// IndexOutOfBoundsException
+//
+//val b = listOf("1", "2")
+//b[3]
+
+// ClassCastException
+//    val cic = "слова"
+//    cic as Number
+
+// IllegalArgumentException
+    //  Array<Int>(-1) { 0 }
+
+// NumberFormatException
+//    val str = "a"
+//    str.toInt()
+
+// IllegalStateException
+//    val nul = null
+//    checkNotNull(nul)
+
+// OutOfMemoryError
+//    val list = mutableListOf<Number>()
+//    while (true) {
+//        list.add(1000 * 1000.00F)
+//    }
+
+// StackOverflowError
+
+    exe1()
+}
+
+fun exe1() {
+    exe1()
+}
