48814f42503096140f75860918a32f276c1fa4f9
petukhov.vladislav <robertlokam@yandex.ru>
2025-11-23 22:29:16 +0300

    lesson22

---
+++ src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson22/Exceptions.kt
@@ -0,0 +1,56 @@
+package ru.stimmax.ru.stimmax.kotlin_course.lesson.lesson22
+
+fun main() {
+    /* throw Exception("Ошибка")
+     throw Error("")
+     OutOfMemoryError() // переполнена память, утечка памяти.
+     StackOverflowError() // переполнен стек вызова функций
+     Exception()
+     IOException()
+     RuntimeException() */
+
+    try {
+        arrayOf("1")[2]
+    } catch (e: IndexOutOfBoundsException) {
+        println(e.message)
+    } catch (e: Exception) {
+        println(e)
+    } finally {
+        // Блок, который выполняется всегда, независимо от наличия исключения
+        println("Всегда выполняется")
+    }
+
+    try {
+        // Код, который может выбросить исключения
+    } catch (e: Exception) {
+        when (e) {
+            is IndexOutOfBoundsException, is NullPointerException -> {
+                println("Обработано исключение типа IndexOutOfBoundsException")
+            }
+
+            is RuntimeException -> {
+                println("Обработано исключение типа RuntimeException")
+            }
+
+            else -> throw e // Перебрасываем исключение дальше, если оно не соответствует обработанным типам
+        }
+    }
+
+}
+
+fun placeOrder(orderDetails: String) {
+    if (orderDetails.isBlank()) {
+        throw OrderException("Детали заказа не указаны.")
+    }
+    // Логика оформления заказа
+    println("Заказ оформлен: $orderDetails")
+}
+
+class CustomException : RuntimeException {
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(cause: Throwable) : super(cause)
+}
+
+
+class OrderException(message: String) : RuntimeException(message)
---
+++ src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson22/Exceptions1.kt
@@ -0,0 +1,45 @@
+package ru.stimmax.ru.stimmax.kotlin_course.lesson.lesson22
+
+fun main() {
+    // val obj = null
+    // obj!!
+
+//    try {
+//        val list = listOf(1, 2, 3)
+//        list[5]
+//    } catch (e: ArrayIndexOutOfBoundsException) {
+//        throw MyExc(e.message)
+//    }
+    //   val a: Any = 45
+    // a as String
+
+//    val str = "qwe"
+//    str.toFloatOrNull()
+//
+//    addMoney(-100)
+
+    // val file = Files("//gnm.com")
+    //file.
+
+//    val a = mutableListOf<Float>()
+//    while (true) {
+//        a.add(Math.PI.toFloat())
+//    }
+//
+//    ex()
+//
+//}
+//
+//fun ex() {
+//    ex()
+
+
+}
+
+class MyExc(mess: String?) : RuntimeException(mess)
+
+//fun addMoney(money: Int) {
+//    check(money > 0) { // требование
+//        "что то"
+//    }
+//}
