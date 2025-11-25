025f9fb99715bfdb40cffcab0f350ed6d1bb4d19
petukhov.vladislav <robertlokam@yandex.ru>
2025-11-25 23:25:03 +0300

    lesson22 HW add 12 fix

--- src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson22/homeworks/MyExcep1.kt
+++ src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson22/homeworks/MyExcep1.kt
@@ -1,19 +1,23 @@
 package ru.stimmax.ru.stimmax.kotlin_course.lesson.lesson22.homeworks
 
+// Создай свой тип исключения в отдельном файле,
+// наследуемый от RuntimeException и принимающий IndexOutOfBoundsException
+// тип в качестве аргумента. Напиши код, который спровоцирует выброс IndexOutOfBoundsException,
+// перехвати его с помощью try-catch и в блоке catch выведи сообщение в консоль и сделай выброс своего типа исключения.
+
+
 class MyExcep1(cause: IndexOutOfBoundsException) : RuntimeException(cause)
 
 fun listIndexOff(list: List<String>, index: Int) {
-    if (list.lastIndex < index) {
-        throw MyExcep1(
-            cause = IndexOutOfBoundsException()
-        )
-    }
+    list[index]
 }
 
 fun main() {
     try {
         listIndexOff(listOf("", "2"), 3)
-    } catch (e: MyExcep1) {
+    } catch (e: IndexOutOfBoundsException) { // тут всегда именно то исключение которое мы ждем в тру
         println("Вышли за рамки списка")
+        throw MyExcep1(e)
     }
+
 }
