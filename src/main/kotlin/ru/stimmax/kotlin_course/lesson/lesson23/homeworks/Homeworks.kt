542c1677eaa3a314ed8f82916f2e376236f01643
petukhov.vladislav <robertlokam@yandex.ru>
2025-11-27 22:39:26 +0300

    lesson23 HW add 1

--- src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson23/homeworks/Homeworks.kt
+++ src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson23/homeworks/Homeworks.kt
@@ -2,6 +2,29 @@ package ru.stimmax.ru.stimmax.kotlin_course.lesson.lesson23.homeworks
 
 fun main() {
 
+    // Создай аналогичную анонимную функцию.
+    fun(list: List<Int>): Int {
+        require(list.isNotEmpty()) { "Пустой получается" }
+        val evenNum = list.filter { it % 2 == 0 }
+        return evenNum.sum() / evenNum.size
+    }
+//Создай аналогичное лямбда выражение с указанием типа.
+    val evenAverage1: (List<Int>) -> Int = { list ->
+        require(list.isNotEmpty()) { "Пустой получается" }
+        val evenNum = list.filter { it % 2 == 0 }
+        evenNum.sum() / evenNum.size
+    }
+
+//Создай лямбда выражение без указания типа.
+    val evenAverage2 = { list: List<Int> ->
+        require(list.isNotEmpty()) { "Пустой получается" }
+        val evenNum = list.filter { it % 2 == 0 }
+        evenNum.sum() / evenNum.size
+    }
+
+    listOf<Int>()
+    val list2 = listOf<Int>(2, 4, 5, 7, 6, 8, 9, 10)
+    println(evenAverage2(list2))
 }
 
 // Создай функцию, которая принимает список чисел и возвращает среднее арифметическое всех чётных чисел этого списка.
@@ -13,8 +36,5 @@ fun evenAverage(list: List<Int>): Int {
     return evenNum.sum() / evenNum.size
 }
 
-val evenAverage1 = fun(list: List<Int>): Int {
-    require(!list.isEmpty()) { "Пустой получается" }
-    val evenNum = list.filter { it % 2 == 0 }
-    return evenNum.sum() / evenNum.size
-}
+// Создай аналогичную анонимную функцию
+
