315fb3e295d7a28ba231458f187247d5018dac82
petukhov.vladislav <robertlokam@yandex.ru>
2025-12-01 10:28:38 +0300

    lesson23 HW add 3 fix

--- src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson23/homeworks/ListNumberFun.kt
+++ src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson23/homeworks/ListNumberFun.kt
@@ -7,7 +7,7 @@ package ru.stimmax.ru.stimmax.kotlin_course.lesson.lesson23.homeworks
 // Создай аналогичное лямбда выражение с указанием типа.
 // Проверь, что лямбда выражение работает правильно с помощью нескольких наборов данных.
 
-// Вариант 1
+// Вариант 1 аналогично 2 варианту
 fun List<Int>.dist(): Set<Int> {
     val list = this
     val list2 = toSet().toList()
@@ -15,15 +15,20 @@ fun List<Int>.dist(): Set<Int> {
     return result.toSet()
 }
 
-// Вариант 2
+// Вариант 2 неверный так как получим пустой список в итоге
 fun List<Int>.dist2(): Set<Int> {
     return (this - toSet().toList()).toSet()
 }
 
 fun main() {
     // Создай аналогичную анонимную функцию.
-    val dist3 = fun List<Int>.(): Set<Int> {
-        return (this - toSet().toList()).toSet()
+    val d1 = fun List<Int>.(): Set<Int> {
+        return toSet().associate { element ->
+            element to this.count { it == element }
+        }.filter { it.value > 1 }
+            .keys
     }
-    dist3(listOf())
+
+    val listt = listOf(1, 2, 2, 3, 3, 3, 4, 5)
+    println(listt.d1())
 }
