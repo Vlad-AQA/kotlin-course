a844a73a345695a9617302f67d5bea283d788f21
petukhov.vladislav <robertlokam@yandex.ru>
2025-12-21 21:56:04 +0300

    lesson29 HW 1..3

--- src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson29/homeworkcs/Employee.kt
+++ src/main/kotlin/ru/stimmax/kotlin_course/lesson/lesson29/homeworkcs/Employee.kt
@@ -1,5 +1,8 @@
 package ru.stimmax.ru.stimmax.kotlin_course.lesson.lesson29.homeworkcs
 
+import com.google.gson.GsonBuilder
+import java.io.File
+
 // Создай дата класс сотрудника компании Employee, содержащий:
 // имя
 // статус трудоустройства (булево)
@@ -10,6 +13,126 @@ package ru.stimmax.ru.stimmax.kotlin_course.lesson.lesson29.homeworkcs
 data class Employee(
     val name: String,
     val status: Boolean,
-    val dataBirth: String,
-    val subordinates: List<Employee>
+    val dateBirth: String,
+    val jobTitle: CharacterTypes,
+    val subordinates: List<Employee?> = emptyList()
 )
+
+// Создай объекты рабочей группы так, чтобы у CTO был PM и CRM, у PM было два тим-лида
+// (на фронт и бэк) и у каждого было по группе сотрудников подходящего для его стека профиля.
+
+fun main() {
+
+    val front = Employee(
+        "Андрей",
+        true,
+        "01.01.1992",
+        CharacterTypes.FRONTEND_DEV,
+        emptyList()
+    )
+
+    val front2 = Employee(
+        "Алексей",
+        true,
+        "07.02.1990",
+        CharacterTypes.FRONTEND_DEV,
+        emptyList()
+    )
+    val back = Employee(
+        "Руслан",
+        true,
+        "27.06.1990",
+        CharacterTypes.BACKEND_DEV,
+        emptyList()
+    )
+    val back2 = Employee(
+        "Сергей",
+        true,
+        "08.08.1999",
+        CharacterTypes.BACKEND_DEV,
+        emptyList()
+    )
+
+    val teamLeadFront = Employee(
+        "Джафар",
+        true,
+        "09.05.1987",
+        CharacterTypes.TEAM_LEAD,
+        listOf(front, front2)
+    )
+
+    val timLidBack = Employee(
+        "Денис",
+        true,
+        "19.03.1988",
+        CharacterTypes.TEAM_LEAD,
+        listOf(back, back2)
+    )
+    val productManager = Employee(
+        "Валера",
+        true,
+        "10.10.1985",
+        CharacterTypes.PM,
+        listOf(timLidBack, teamLeadFront)
+    )
+
+    val crm = Employee(
+        "Павел",
+        true,
+        "11.11.1986",
+        CharacterTypes.CRM,
+        emptyList()
+    )
+
+    val cto = Employee(
+        "Влад",
+        true,
+        "06.07.1994",
+        CharacterTypes.CTO,
+        listOf(crm, productManager)
+    )
+
+    ex(cto)
+
+}
+
+// Создай функцию в которой:
+// сериализуй CTO в текст с настройкой prettyPrinting
+// текст запиши в файл в корне проекта.
+
+fun ex(cto: Employee) {
+
+    val gson = GsonBuilder()
+        .setPrettyPrinting()
+        .create()
+    gson.toJson(cto)
+        .also {
+            File("src/main/resources/CTO.json")
+                .writeText(it)
+        }
+
+}
+
+fun ex1(file: File) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
