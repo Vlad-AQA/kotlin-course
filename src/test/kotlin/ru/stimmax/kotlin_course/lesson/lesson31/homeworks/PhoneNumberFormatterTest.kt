65b976c031303d2876a3188a8623ee32870f8622
petukhov.vladislav <robertlokam@yandex.ru>
2025-12-29 13:49:43 +0300

    lesson31 add tests

--- src/test/kotlin/ru/stimmax/kotlin_course/lesson/lesson31/homeworks/PhoneNumberFormatterTest.kt
+++ src/test/kotlin/ru/stimmax/kotlin_course/lesson/lesson31/homeworks/PhoneNumberFormatterTest.kt
@@ -1,9 +1,50 @@
 package ru.stimmax.kotlin_course.lesson.lesson31.homeworks
 
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import ru.stimmax.ru.stimmax.kotlin_course.lesson.lesson31.homeworks.PhoneNumberFormatter
+import kotlin.test.assertEquals
 
 class PhoneNumberFormatterTest {
+
     @ParameterizedTest
-    @MethodSource("")
+    @MethodSource("positiveNumberData")
+    fun `should positive format phone number`(
+        number: String
+    ) {
+        val phoneNumberFormatter = PhoneNumberFormatter(number)
+        assertEquals("+7 (922) 941-11-11", phoneNumberFormatter.getNumber())
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("negativeNumberData")
+    fun `should negative format phone number`(
+        number: String
+    ) {
+        val phoneNumberFormatter = PhoneNumberFormatter(number)
+        assertThrows(IllegalArgumentException::class.java) { phoneNumberFormatter.getNumber() }
+
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun positiveNumberData() = listOf(
+            "8 (922) 941-11-11",
+            "79229411111",
+            "+7 922 941 11 11",
+            "9229411111",
+            "abc +7 922 941 11 11",
+        )
+
+        @JvmStatic
+        fun negativeNumberData() = listOf(
+            "12345",
+            "+1 (922) 941-11-11"
+        )
+
+    }
+
 }
